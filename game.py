35215656dd2464e3fccca8703947f0a99cfec2fe
purbayan2014 <purbayan2014@gmail.com>
2022-10-16 17:11:38 +0530

    refactored

--- game.py
+++ game.py
@@ -1,102 +1,111 @@
-from email import message
-from http import client
+import discord
+import os 
+import random
+from discord.utils import get 
+from discord.ext import commands
 
 
-class tic_tac:
-    
-    def __init__():
-        board = board=[0,0,0,0,0,0,0,0,0]
-    
-    # Displaying the current state of the board
-    @client.command()
-    async def display(self, ctx, board):
-        message = 'Current State of Board:'
-        for i in range(0,9):
-            if ((i > 0) and (i%3) == 0):
-                message = message + '\n'
-            match(board[i]):
-                case  0  : message += '- '
-                case  1  : message += 'O '
-                case -1  : message += 'X '
-            message += '\n\n'
-        await ctx.send(message)
-    #This function takes the user move as input and make the required changes on the board.    
-    @client.command()
-    async def User1Turn(self, ctx ,board):
-        await ctx.send('Enter X\'s position from [1...9]:')
-        if(board[ctx-1] != 0):
-            await ctx.send('Wrong move!!! Enter again ')
-            self.User1Turn(board)
-        board[ctx-1] = -1
-    
-    def analyzeboard(board):
-        def analyzeboard(board):
-        cb=[[0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]]
+client = commands.Bot(intents=discord.Intents.all(),command_prefix='/') 
 
-        for i in range(0,8):
-            if(board[cb[i][0]] != 0 and
-            board[cb[i][0]] == board[cb[i][1]] and
-            board[cb[i][0]] == board[cb[i][2]]):
-                return board[cb[i][2]];
+board=[0,0,0,0,0,0,0,0,0]
+    
+# Displaying the current state of the board
+@client.command()
+async def display(ctx):
+    message = 'Current State of Board:'
+    for i in range(0,9):
+        if ((i > 0) and (i%3) == 0):
+            message = message + '\n'
+        match(board[i]):
+            case  0  : message += '- '
+            case  1  : message += 'O '
+            case -1  : message += 'X '
+        message += '\n\n'
+    await ctx.send(message)
+    
+    
+#This function takes the user move as input and make the required changes on the board.    
+@client.command()
+async def p1turn(ctx, message):
+    
+    await ctx.send('Enter X\'s position from [1...9]:')
+    p_moves = message.content
+    if(board[p_moves-1] != 0):
+        await ctx.send('Wrong move!!! Enter again ')
+        p1turn(ctx,board)
+    board[p_moves-1] = -1
+    
+# def analyzeboard(self):
+def analyzeboard():
+    cb=[[0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]]
+
+    for i in range(0,8):
+        if(board[cb[i][0]] != 0 and
+        board[cb[i][0]] == board[cb[i][1]] and 
+        board[cb[i][0]] == board[cb[i][2]]):
+            return board[cb[i][2]];
+    return 0
+
+#minmax
+def minimax(player):
+    x = analyzeboard()
+    if(x != 0):
+        return (x * player)
+    pos = -1
+    value = -2
+    for i in range(0,9):
+        if(board[i] == 0):
+            board[i] = player
+            score = -(minimax((player*-1)))
+            if(score > value):
+                value = score
+                pos = i
+            board[i] = 0
+    if(pos == -1):
         return 0
+    return value
 
-    #minmax
-    def minimax(self, board, player):
-        x = analyzeboard(board)
-        if(x != 0):
-            return (x * player)
-        pos = -1
-        value = -2
-        for i in range(0,9):
-            if(board[i] == 0):
-                board[i] = player
-                score = -(self.minimax(board,(player*-1)))
-                if(score > value):
-                    value = score
-                    pos = i
-                board[i] = 0
-        if(pos == -1):
-            return 0
-        return value
+def CompTurn():
+    pos = -1
+    value = -2
+    for i in range(0,9):
+        if(board[i] == 0):
+            board[i] = 1
+            score = -(minimax(-1))
+            board[i] = 0
+            if(score > value):
+                value = score
+                pos = i
 
-    def CompTurn(board):
-        pos = -1
-        value = -2
-        for i in range(0,9):
-            if(board[i] == 0):
-                board[i] = 1
-                score = -minimax(board, -1)
-                board[i] = 0
-                if(score > value):
-                    value = score
-                    pos = i
+    board[pos] = 1
+
+@client.command()
+async def play_game(ctx):
+    for i in range(0,9):
+        if(analyzeboard() != 0):
+            break
+        if((i + 1) % 2 == 0):
+            CompTurn()
+        else:
+            display()
+            p1turn()
     
-        board[pos] = 1
+    x=analyzeboard()
+    if(x==0):
+        display()
+        print("Draw!!!")
+    if(x==-1):
+        display()
+        print("X Wins!!! O Loose !!!")
+    if(x==1):
+        display()
+        print("O Wins!!! X Loose !!!")
 
-    async def play_game(self):
-        for i in range(0,9):
-            if(analyzeboard(board) != 0):
-                break
-            if((i + 1) % 2 == 0):
-                CompTurn(board)
-            else:
-                self.display(board)
-                self.User1Turn(board)
-        
-        x=analyzeboard(board)
-        if(x==0):
-            self.display(board)
-            print("Draw!!!")
-        if(x==-1):
-            self.display(board)
-            print("X Wins!!! O Loose !!!")
-        if(x==1):
-            self.display(board)
-            print("O Wins!!! X Loose !!!")
+client.run('MTAzMDg3NjQzNjU2NDgxOTk2OA.GlwBGj.uvNKFBAhfB5R4R_GwADJpsxr2-SX2QfNa_uiME')
